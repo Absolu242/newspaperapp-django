651e4cb26ea569f475f8da9cb0fa2dff629f0799
Rahan Bouess <wangleibniz01@gmail.com>
2022-11-09 14:37:11 +0100

    user auth and testing

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,13 +3,21 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 
-class CustomUserCreationForm(UserChangeForm):
+class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",)
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = fields = (
+            "username",
+            "email",
+            "age",
+        )
